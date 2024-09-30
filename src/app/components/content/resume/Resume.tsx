6ab6d65829b423815dc953c553fec0b19d5b7482
rosyfi <filocomo.rosy@gmail.com>
2024-09-30 20:35:37 +0200

    added CV

--- src/app/components/content/resume/Resume.tsx
+++ src/app/components/content/resume/Resume.tsx
@@ -4,10 +4,10 @@ import styles from "../../../styles/content/resume/Resume.module.css";
 const Resume = () => {
   return (
     <div className={styles.container}>
-      <iframe src="/pdf/Rossella_Filocomo_CV.pdf" width="100%" height="600px" />
+      <iframe src="/pdf/CV_Rossella_Filocomo.pdf" width="100%" height="600px" />
       <a href="/pdf/CV_Rossella_Filocomo.pdf" download>
         <button className={styles.button}>
-          <i className="bi bi-download"> Download Rossella{"'"}s CV (127 KB)</i>
+          <i className="bi bi-download"> Download Rossella{"'"}s CV (780 KB)</i>
         </button>
       </a>
     </div>
